23d17095521ce749b4bf8231c8bccf442fda2978
Teun van Vegchel <teun@teunvanvegchel.nl>
2017-06-01 11:38:24 +0200

    Initial draft of simple flow mechanism

---
+++ src/flow/flow.js
@@ -0,0 +1,37 @@
+'use strict'
+
+class Flow {
+
+  constructor (logic) {
+    this.logic = logic
+    this.items = []
+    this.state = {}
+  }
+
+  run () {
+    return logic(this)
+  }
+
+  ask (question) {
+    return new Promise((resolve, reject) -> {
+      this.items.push(question)
+      if (flow.hasValue(question)) {
+        resolve(flow.value(question))
+      }
+    })
+  }
+
+  value (question) {
+    return this.state[question.getId()]
+  }
+
+  hasValue (question) {
+    return value(question) != null && value(question) != undefined
+  }
+
+}
+
+
+function (flow) {
+  return flow.ask(questions.name())
+}
---
+++ src/flow/question.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class Question {
+
+  constructor (id, title, answers) {
+    this.id = id
+    this.title = title
+    this.answers = answers
+  }
+
+}
